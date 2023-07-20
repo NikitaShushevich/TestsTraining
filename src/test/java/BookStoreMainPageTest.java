e6c548e2bbbf35eb60664f7420bf0c19b5ca22cb
Nikita <nikita.shushevich@gmail.com>
2023-07-20 13:21:25 +0300

    added new classes and modified pom and others

---
+++ src/test/java/BookStoreMainPageTest.java
@@ -0,0 +1,9 @@
+import org.testng.annotations.Test;
+
+public class BookStoreMainPageTest extends TestInit{
+
+    @Test
+    public void clickOnBook(){
+
+    }
+}
---
+++ src/test/java/TestInit.java
@@ -0,0 +1,22 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public class TestInit {
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setup(){
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+    }
+    @AfterMethod
+    public void tearDown(){
+        driver.quit();
+    }
+
+    public void goToDemoQa(){driver.get("http://www.demoqa.com");}
+    public void goToBookStore(){driver.get("https://demoqa.com/books");}
+}
